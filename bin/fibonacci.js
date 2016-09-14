c9690499aadfdb2d2160ceef65cd4e6c2f0c0f8b
Jeffrey Horn <jeffh@generalassemb.ly>
2016-09-14 14:12:53 -0400

    Add code-along solution for sum of fibs

--- bin/fibonacci.js
+++ bin/fibonacci.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+
+// sum of all the elements of the fibs array
+const sum = (a, b) => {
+  return a + b;
+};
+
+let sumFibs = fibs.reduce(sum, 0);
+
+console.log('The sum of the first ten fibonacci numbers is ', sumFibs);
+
--- bin/fibonacci.rb
+++ bin/fibonacci.rb
@@ -1,1 +1,7 @@
 fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+# sum of all the elements of the fibs array
+sum_fibs = fibs.reduce(0, :+)
+
+puts "The sum of the first ten fibonacci numbers is #{sum_fibs}"
+
